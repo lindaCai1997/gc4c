372106996b7d5d680efa16663f6461474548581f
Faisal <faisal.abujabal@gmail.com>
2016-03-05 15:53:29 -0600

    renamed the data structure to _metaData

--- inc/malloc.c
+++ inc/malloc.c
@@ -4,4 +4,31 @@
  * malloc.h implementation
  */
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "malloc.h"
+#include "dataStructure.h"
+
+#undef malloc
+#undef calloc
+#undef realloc
+#undef free
+
+void* gc_malloc(size_t size){
+    void* userData = malloc(size);
+    Node_insert(_metaData, userData);
+    return userData;
+}
+
+void* gc_realloc(void* ptr, size_t size){
+    return NULL;
+}
+
+void* gc_free(void* ptr){
+    return NULL;
+}
+
+void* gc_calloc(size_t nmemb, size_t size){
+    return NULL;
+}
--- inc/malloc.h
+++ inc/malloc.h
@@ -3,6 +3,16 @@
  *
  * Header file for malloc functions
  *
- * /
+ */
 
+#define malloc(size) gc_malloc(size)
+#define realloc(ptr, size) gc_malloc(ptr, size)
+#define calloc(nmemb, size) gc_calloc(nmemb, size)
+#define free(ptr) gc_free(ptr)
 
+#include <stddef.h>
+
+void* gc_malloc(size_t size);
+void* gc_realloc(void* ptr, size_t size);
+void* gc_free(void* ptr);
+void* gc_calloc(size_t nmemb, size_t size);
