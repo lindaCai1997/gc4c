0e457b1fdb516b725e11d0ac17f2e8116d2262e7
Linda Cai <tcai4@illinois.edu>
2016-03-05 14:21:29 -0600

    Create mark.c

---
+++ inc/mark.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dataStructure.h>
+
+
+extern end;
+size_t   heap_top;
+size_t*  stack_bottom;
+size_t*  stack_top;
+
+void  mark_on_stack(DataStructure *heapdata){
+    find_stack_bottom();
+    size_t i;
+    stack_top = &i;
+    size_t* address = stack_top;
+    for (; address < stack_bottom; address ++){
+        mark(address);
+    }
+
+}
+
+int mark(size_t* sp){
+
+    if (*sp >= end && *sp <= heap_top){
+        Node_insert(heapdata, (void*)(*sp));
+        return 1;
+    } 
+    return 0;
+}
+
+void find_stack_bottom(){
+    FILE* statfp = fopen("/proc/self/stat", "r");
+    assert(statfp != NULL);
+    fscanf(statfp, "%*d %*s %*c %*d %*d %*d %*d %*d %*u "
+           "%*lu %*lu %*lu %*lu %*lu %*lu %*ld %*ld "
+           "%*ld %*ld %*ld %*ld %*llu %*lu %*ld "
+           "%*lu %*lu %*lu %lu", stack_bottom);
+    fclose(statfp); 
+}   
+    
+int main(){ 
+    heap_top = (size_t)sbrk(0);
+    DataStructure* heapdata;
+    DataStructure_init(heapdata);
+    mark_on_stack(heapdata);
+    return 0;
+}
