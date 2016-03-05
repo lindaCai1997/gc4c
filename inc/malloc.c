b261a51aa5ded8f48621940bc549b76d12e3cec8
Faisal <faisal.abujabal@gmail.com>
2016-03-05 16:02:11 -0600

    merge calloc, malloc, and realloc into one file

--- inc/malloc.c
+++ inc/malloc.c
@@ -22,7 +22,26 @@ void* gc_malloc(size_t size){
 }
 
 void* gc_realloc(void* ptr, size_t size){
-    return NULL;
+    if(ptr == NULL)
+        return NULL;
+    if(size == 0)
+    {
+        gc_free(ptr);
+        return NULL;
+    }
+    Node* node = dataStructure_findNode(ds, ptr);
+    size_t oldsize = node->size;
+    size_t newsize = oldsize > size ? size : oldsize; 
+
+    Node_remove(ds, ptr);
+
+    void* newptr = realloc(ptr, size);
+    if(newptr == NULL)
+        return NULL;
+
+    Node_insert(ds, newptr, newsize);
+
+    return newptr;
 }
 
 void* gc_free(void* ptr){
@@ -30,5 +49,8 @@ void* gc_free(void* ptr){
 }
 
 void* gc_calloc(size_t nmemb, size_t size){
-    return NULL;
+    void* ptr = calloc(num, size);
+    if(ptr != NULL)
+        Node_insert(ds, ptr, size);
+    return ptr;
 }
